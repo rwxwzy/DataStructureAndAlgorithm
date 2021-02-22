e7e2cf651ddca001ca1675d73bcd0166b9d0462d
rossw <ross.wang@nokia-sbell.com>
2021-02-22 23:04:16 +0800

    Add add node to list

--- list/list.c
+++ list/list.c
@@ -1,3 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "list.h"
+
+typedef struct listNode {
+	void *value;
+	struct listNode *next;
+} listNode;
+
+typedef struct list {
+	listNode *head;
+	listNode *tail;
+	unsigned long len;
+} list;
+
+list* listCreate() {
+    struct list *list;
+    if ((list = (struct list*) malloc(sizeof(struct list))) == NULL) {
+	    return NULL;
+    }
+	
+	list->head = NULL;
+	list->tail = NULL;
+	list->len = 0;
+}
+
+void emptyList(list *list) { 
+}
+
+list* listAddNodeHead(list *list, void *value) {
+	listNode *node;
+	if ((node = (listNode*)malloc(sizeof(listNode))) == NULL) {
+		return NULL;
+	}
+	
+	node->value = value;
+	if (list->len == 0) { // set tail
+		list->head = list->tail = node;
+		node->next = NULL;
+	} else {
+		node->next = list->head;
+	    list->head = node;
+	}
+
+	list->len ++;
+	
+	return list;
+}
+
+list *listAddNodeTail(list *list, void *value) {
+	listNode *node;
+	if ((node = (listNode *)malloc(sizeof(listNode))) == NULL) {
+		return NULL;
+	}
+	
+	//each element in list, head, tail, node 
+	node->value = value;
+	if (list->len == 0) {
+		list->head = list->tail = node;
+		node->next = NULL;
+	} else {
+		list->tail->next = node;
+		list->tail = node;
+		node->next = NULL;
+	}
+	
+	list->len ++;
+}
+
+
+void main() {
+	
+}
+
+
+
 
 
---
+++ list/list.h
@@ -0,0 +1,8 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+#endif
