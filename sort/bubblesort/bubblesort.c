ca434944598373bc720b15b3f26fe2d1eebabc15
rossw <ross.wang@nokia-sbell.com>
2021-03-06 09:35:22 +0800

    Modify

--- sort/bubblesort/bubblesort.c
+++ sort/bubblesort/bubblesort.c
@@ -4,9 +4,12 @@ void bubblesort(int a[], int size) {
     int i, j;
     int changed;
     int tmp;
+    int step = 0;
     for (i = 0; i < size - 1; i ++) {
         changed = 0;
-        for (j = 0; j < size - 1; j ++) {
+	// size - 1 - i, is already sorted
+        for (j = 0; j < size - 1 - i; j ++) {
+        // for (j = 0; j < size - 1; j ++) {
             
             if (a[j] > a[j + 1]) {
                 tmp = a[j + 1];
@@ -14,6 +17,7 @@ void bubblesort(int a[], int size) {
                 a[j] = tmp;
                 changed = 1;
             }
+	    step ++;
         }
         
         if (changed == 0) {
@@ -21,12 +25,14 @@ void bubblesort(int a[], int size) {
         }
         
     }
+
+    printf("step: %d\n", step);
     
 }
 
 
 void main() {
-    int a[10] = { 9, 8, 7, 6, 5, 4,3, 2, 1, 0};
+    int a[11] = { 9, 8, 7, 6, 5, 4,3, 3,  2, 1, 0};
     bubblesort(a, sizeof(a)/ sizeof(a[0]));
     int i;
     for (i = 0; i < sizeof(a)/ sizeof(a[0]); i ++) {
