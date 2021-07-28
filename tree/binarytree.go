88f92321d9a3f9ada69273344be3fe856dce70d3
rossw <ross.wang@nokia-sbell.com>
2021-07-28 23:44:35 +0800

    Add binary tree

--- tree/binarytree.go
+++ tree/binarytree.go
@@ -17,13 +17,13 @@ type BinaryTree struct{
 func (node *Node)AddNode(new *Node) {
     if node.data > new.data {
         if node.left != nil {
-            node.left.Add(new)
+            node.left.AddNode(new)
         } else {
             node.left = new
         }
     } else {// right
         if node.right != nil {
-            node.right.Add(new)
+            node.right.AddNode(new)
         } else {
             node.right = new
         }
@@ -38,15 +38,51 @@ func (bt *BinaryTree)AddNode(data int) {
         bt.root.AddNode(n)
     }
 }
-func (bt *BinaryTree)PrintBefore(){
-    
+func (bt *BinaryTree)PreOrder(node *Node){
+    if node == nil {
+        return
+    }
+    bt.PreOrder(node.left)
+    fmt.Println(node.data)
+    bt.PreOrder(node.right)
 }
+
+func(bt *BinaryTree)PostOrder(node *Node) {
+    if node == nil {
+        return
+    }
+    bt.PostOrder(node.right)
+    fmt.Println(node.data)
+    bt.PostOrder(node.left)
+}
+
+func(bt *BinaryTree)InOrder(node *Node) {
+    if node == nil {
+        return
+    }
+    fmt.Println(node.data)
+    bt.InOrder(node.right)
+    
+    bt.InOrder(node.left)
+}
+
+// TODO: remove
+
 
 func main() {
     b := &BinaryTree{}
     b.AddNode(10)
     b.AddNode(9)
+    b.AddNode(12)
+    b.AddNode(13)
+    b.AddNode(9)
     b.AddNode(11)
+    fmt.Println("PreOrder---------------")
+    b.PreOrder(b.root)
+    fmt.Println("PostOrder---------------")
+    b.PostOrder(b.root)
+    fmt.Println("InOrder---------------")
+    b.InOrder(b.root)
 }
 
 
